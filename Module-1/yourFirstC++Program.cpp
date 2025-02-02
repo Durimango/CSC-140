d0b50f00296939a6d32b65e37324366fc8325d86
Durimango <108048960+Durimango@users.noreply.github.com>
2025-02-02 16:24:04 -0600

    firstCommit
    I setup the assigned program and validated all of the outputs.

---
+++ Module-1/yourFirstC++Program.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int var1 = 42;
+    float var2 = 3.14;
+    double var3 = 2.71828;
+    char var4 = 'C';
+    bool var5 = true;
+    std::string var6 = "Hello, World";
+
+    std::cout << "Integer: " << var1 << '\n';
+    std::cout << "Float: " << var2 << '\n';
+    std::cout << "Double: " << var3 << '\n';
+    std::cout << "Character: " << var4 << '\n';
+    std::cout << "Boolean: " << var5 << '\n';
+    std::cout << "String: " << var6;
+
+    std:: system("pause>0");
+
+    return 0;
+}
