7fe21952a69d6276449304ac4c27516cdbe2c17e
Durimango <108048960+Durimango@users.noreply.github.com>
2025-02-20 21:08:06 -0600

    Renaming and adding files
    Added BasicArithmetic.cpp and renamed another file to MyFirstC++Program.cpp

---
+++ Module-2/BasicArithmetic.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+    int num1 = 42;
+    int num2 = 3;
+
+    std::cout << "Starting variables " << "x = " << num1 << " and y = " << num2 << "." << '\n';
+    std::cout << "Addition: " << num1+num2 << '\n';
+    std::cout << "Subtraction: " << num1-num2 << '\n';
+    std::cout << "Multiplication: " << num1*num2 << '\n';
+    std::cout << "Division: " << num1/num2 << '\n';
+    std::cout << "Modulo: " << num1%num2 << '\n';
+
+    std:: system("pause>0");
+
+    return 0;
+}
