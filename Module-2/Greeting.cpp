a867a2bdcd5f1184fd9cb600a34c78539c53057d
Durimango <108048960+Durimango@users.noreply.github.com>
2025-02-22 22:07:19 -0600

    Adding Greeting.cpp
    Added Greeting.cpp

---
+++ Module-2/Greeting.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    
+    std::string userName = "";
+
+    std::cout << "Enter your name: " << '\n';
+    std::cin >> userName;
+    std::cout << "Well hello, " << userName << " so glad you're here!";
+
+    std:: system("pause>0");
+
+    return 0;
+}
