b0228ba96520f3c43d8a11b21b586c06417ede6c
Durimango <108048960+Durimango@users.noreply.github.com>
2025-03-03 21:43:03 -0600

    Created Even or Odd
    This is a simple program to tell if an integer is even or odd.
    
    Unfortunately it does not work properly if you input something that is not an integer.

---
+++ Module-3/OddOrEven.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+//declaring variables
+int userInput;
+
+int main() {
+
+    //display a message and take user input
+    std::cout << "Hi there, I'm Even Steven. Please input an integer: ";
+    std::cin >> userInput;
+
+    //check to see if the user input is divisble by 2. if it is display the first message, if not do the second. (this unfortunately does not account for the user not entering an integer.)
+    if (userInput % 2 == 0) {
+        std::cout << "Oh good it's even. My favorite!";
+    } else {
+        std::cout << "Ugh, I hate odd numbers. Try giving that to Todd.";
+    }
+    std:: system("pause>0");
+    return 0;
+}
