ba863e0bd5e1e4a15c6b4b2b5ca63d9552ff837e
Durimango <108048960+Durimango@users.noreply.github.com>
2025-03-29 18:48:40 -0500

    Updated Module 6 Menu Driven Calculator
    Completed and committing homework assignment.

---
+++ Module-6/MenuDrivenCalculator.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+
+// declaring variables
+int userOption = 0;
+double userNumber1 = 0;
+double userNumber2 = 0;
+double userAnswer = 0;
+bool running = true;
+
+//declaring menu function, this displays the menu to the user and let's them input an option
+void menu(){
+    std::cout << "===== Calculator Menu =====" << "\n"
+    << "1. Addition" << "\n" << "2. Subtraction" << "\n" << "3. Multiplication" << "\n" << "4. Division" << "\n"
+    << "5. Modulo" << "\n" << "6. Exit" << "\n" << "Choose an option: ";
+    std::cin >> userOption;
+}
+
+//declaring userInput function, this gives the user two inputs for their numbers to be operated on
+void userInput(){
+    std::cout << "First number: " << "\n";
+    std::cin >> userNumber1;
+    std::cout << "Second number: " << "\n";
+    std::cin >> userNumber2;
+}
+
+//declaring operation function, this displays the product of the user's numbers
+void operation(){
+    std::cout << "Answer: " << userAnswer << "\n";
+    std::system("pause>0");
+}
+
+//declaring the modulo functio, this converts numbers to integers and operates
+//modulo is a special case that doesn't work with doubles
+void modulo(int x, int y){
+    userAnswer = x % y;
+}
+
+int main(){
+    while (running == true){
+        //displays menu
+        menu();
+
+        //if the user inputs a number outside the range of the menu they're asked to enter a different one
+        if (userOption>=1 && userOption<=6){
+            //this switches between cases corresponding with the menu
+            switch (userOption){
+                //addition
+                case 1:
+                    userInput();
+                    userAnswer = userNumber1 + userNumber2;
+                    operation();
+                    break;
+
+                //subtraction
+                case 2:
+                    userInput();
+                    userAnswer = userNumber1 - userNumber2;
+                    operation();
+                    break;
+                
+                //multiplication
+                case 3:
+                    userInput();
+                    userAnswer = userNumber1 * userNumber2;
+                    operation();
+                    break;
+
+                //division
+                case 4:
+                    userInput();
+                    //if the user tries to divide by 0 it asks them to try again
+                    if (userNumber2 != 0) {
+                        userAnswer = userNumber1 / userNumber2;
+                        operation();
+                    } else {
+                        std::cout << "You cannot divide by 0, try again." << '\n';
+                        std::system("pause>0");
+                    }
+                    break;
+
+                //modulo
+                case 5:
+                    userInput();
+                    ////if the user tries to divide by 0 it asks them to try again
+                    if (userNumber2 != 0) {
+                        modulo(userNumber1, userNumber2);
+                        operation();
+                    } else {
+                        std::cout << "You cannot divide by 0, try again." << '\n';
+                        std::system("pause>0");
+                    }
+                    break;
+                
+                //exiting
+                case 6:
+                    running = false;
+                    std::cout << "Exiting program...";
+                    std::system("pause>0");
+                }
+        } else {
+            std::cout << "Please input a valid number." << "\n";
+            std::system("pause>0");
+            menu();
+        }
+    }
+}
